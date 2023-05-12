6e666ac95fa4e67f2cf868a0993e0daa06a313f2
sabrina bendraoua <sabbendra@gmail.com>
2023-05-12 19:28:12 +0200

    pages Pokemons and Types

--- src/pages/Pokemons.jsx
+++ src/pages/Pokemons.jsx
@@ -4,13 +4,13 @@ import axios from "axios";
 
 const Pokemons = () => {
   const [data, setData] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     try {
       const fetchData = async () => {
         const response = await axios.get("https://pokeapi.co/api/v2/pokemon");
-        console.log(response.data); // j'ai bien ma data
+        // console.log(response.data); // j'ai bien ma data
         setData(response.data.results);
         setIsLoading(false);
       };
@@ -28,10 +28,15 @@ const Pokemons = () => {
       <h1>Pokemons</h1>
       <div>
         {data.map((pokemon, index) => {
+          // console.log(pokemon);
+          const urlPokemon = pokemon.url.split("/")[6];
+          console.log(urlPokemon);
           return (
             <div key={index}>
               <p>{pokemon.name}</p>
-              {/* <img src={url} alt="pokemon" /> */}
+              <img
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${urlPokemon}.png `}
+              />
             </div>
           );
         })}
--- src/pages/Types.jsx
+++ src/pages/Types.jsx
@@ -1,5 +1,44 @@
+import { useState, useEffect } from "react";
+// import { Link } from "react-router-dom";
+import axios from "axios";
+import "../assets/Home.css";
+
 const Types = () => {
-  return <h1>types</h1>;
+  const [data, setData] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    try {
+      const fetchData = async () => {
+        const response = await axios.get("https://pokeapi.co/api/v2/type");
+        console.log(response.data); // j'ai bien ma data
+        setData(response.data.results);
+        setIsLoading(false);
+      };
+
+      fetchData();
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
+  return isLoading ? (
+    <p>Loading...</p>
+  ) : (
+    <div>
+      <h1>Types</h1>
+      <main classnamme="type-main">
+        {data.map((type, index) => {
+          // console.log(type);
+          return (
+            <div key={index} className="type-div">
+              <p className="type-p">{type.name}</p>
+            </div>
+          );
+        })}
+      </main>
+    </div>
+  );
 };
 
 export default Types;
